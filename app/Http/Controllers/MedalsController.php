8cc6008bf7f9520012a7f8603678bf82f300d985
Daniil <m05419325@gmail.com>
2024-09-12 19:54:59 +0300

    Реалтзовал часть роутов и начал вёрстку

--- app/Http/Controllers/MedalsController.php
+++ app/Http/Controllers/MedalsController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 use App\Models\Medals;
-
 use Illuminate\Http\Request;
 
 class MedalsController extends Controller
 {
     public function show() {
-        /*$medals = Medals::all();
-        dump($medals);*/
+        $medals = Medals::find(1);
+        //dump($medals->name);
+        return view('medals.show', ['name' => $medals->name]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,17 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
+use App\Models\Countries;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $country = new Countries;
+        $country->name = 'Russia';
+        $country->save();
     }
 }
